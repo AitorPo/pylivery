b58ea44062901499cb76a786327606b9414a010c
Aitor Poquet <aitorpoquet@rudo.es>
2023-09-17 11:26:14 +0200

    Add tests

---
+++ clients/enums.py
@@ -0,0 +1,7 @@
+import enum
+
+
+class ClientsEnum(enum.Enum):
+    GLOVO = 'glovo'
+    CATCHER = 'catcher'
+    UBER = 'uber'
---
+++ functions.py
@@ -0,0 +1,16 @@
+from typing import Union
+
+from clients.enums import ClientsEnum
+from clients.catcher import catcher_client, CatcherClient
+from clients.glovo import glovo_v2_client, GlovoV2Client
+from clients.ubereats import uber_eats_client, UberEatsClient
+
+CLIENTS = {
+    ClientsEnum.GLOVO.value: glovo_v2_client,
+    ClientsEnum.CATCHER.value: catcher_client,
+    ClientsEnum.UBER.value: uber_eats_client
+}
+
+
+def get_delivery_client(client: str) -> Union[CatcherClient, GlovoV2Client, UberEatsClient]:
+    return CLIENTS[client]
